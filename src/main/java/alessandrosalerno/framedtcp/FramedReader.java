ba8bb6b2055de3215042d723c8369edc9a42e995
Alessandro-Salerno <OxyGen05@outlook.it>
2024-07-14 16:34:05 +0200

    bug fixed?

--- src/main/java/alessandrosalerno/framedtcp/FramedReader.java
+++ src/main/java/alessandrosalerno/framedtcp/FramedReader.java
@@ -1,43 +1,43 @@
 package alessandrosalerno.framedtcp;
 
 import java.io.IOException;
-import java.io.Reader;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class FramedReader {
-    private final Reader reader;
+    private final InputStream inputStream;
     private final Charset charset;
     private final FrameSizeValidator frameSizeValidator;
 
-    public FramedReader(Reader reader,
+    public FramedReader(InputStream inputStream,
                         Charset charset,
                         FrameSizeValidator frameSizeValidator) {
 
-        this.reader = reader;
+        this.inputStream = inputStream;
         this.charset = charset;
         this.frameSizeValidator = frameSizeValidator;
     }
 
-    public FramedReader(Reader reader,
+    public FramedReader(InputStream inputStream,
                         Charset charset) {
 
-        this.reader = reader;
+        this.inputStream = inputStream;
         this.charset = charset;
         this.frameSizeValidator = new DefaultFrameSizeValidator();
     }
 
-    public FramedReader(Reader reader,
+    public FramedReader(InputStream inputStream,
                         FrameSizeValidator frameSizeValidator) {
 
-        this.reader = reader;
+        this.inputStream = inputStream;
         this.charset = StandardCharsets.UTF_8;
         this.frameSizeValidator = frameSizeValidator;
     }
 
-    public FramedReader(Reader reader) {
-        this.reader = reader;
+    public FramedReader(InputStream inputStream) {
+        this.inputStream = inputStream;
         this.charset = StandardCharsets.UTF_8;
         this.frameSizeValidator = new DefaultFrameSizeValidator();
     }
@@ -68,9 +68,7 @@ public class FramedReader {
 
     private byte[] readSegment(int size) throws IOException {
         byte[] bytes = new byte[size];
-        for (int i = 0; i < bytes.length; i++)
-            bytes[i] = (byte) this.reader.read();
-
+        this.inputStream.read(bytes, 0, size);
         return bytes;
     }
 }
